7e047c11592f889f26d325b90785bab57353b16f
SonLee <sonthanhle.mta@gmail.com>
2023-11-08 20:06:27 +0700

    main/create

---
+++ Test Git Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Git Demo
+//
+//  Created by Le Thanh Son on 08/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var myView: UIView!
+    
+}
+
